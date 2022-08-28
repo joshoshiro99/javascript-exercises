689b5b087e8afffda05784ae2688256dd9914da3
Josh Oshiro <joshoshiro99@gmail.com>
2022-08-28 12:32:20 -0400

    Add repeatString functionality

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, timesToRepeat) {
+ let returnString="";
+ if (timesToRepeat >= 0) { 
+	 for (let i = 0; i < timesToRepeat; i++){
+		 returnString += string;
+ 	}
+ 	return returnString;
+ }
+  else if (timesToRepeat < 0) {
+	 return "ERROR";
+  }
+}
 
 // Do not edit below this line
 module.exports = repeatString;

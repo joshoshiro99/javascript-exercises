e98649ba8818fff016e4dfff0cd6d728ef06f2aa
Josh Oshiro <joshoshiro99@gmail.com>
2022-08-29 19:14:59 -0400

    Add array swap method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+//testing the javscript array immutability
+//will swap the array indices first
+//if after they fail then will recreate with a new string backwards
 
+const reverseString = function(inputString) {
+ let stringArray = inputString.split("");
+ for (let currentPosition = 0; i < inputString.length/2; i++) {
+	 let endingCharacter = inputString-i;
+	 stringArray[i]=stringArray[endingCharacter];
+ }
+ return stringArray.join();
 };
 
 // Do not edit below this line

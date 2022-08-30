0b02185311bb3f6c44b03a99e9dbd36722b4f3aa
Josh Oshiro <joshoshiro99@gmail.com>
2022-08-29 20:59:40 -0400

    Add remove functionality

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,36 @@
-const removeFromArray = function() {
+//how to solve remove from array: 
+//
+//iterate through array until item at index == searchItem
+//create temporary fifo queue
+//loop
+//remove last element from array
+//push to end of temp queue
+//get to searchItem
+//remove searchItem
+//shift items from queue back to array
+//return array
 
+
+const removeFromArray = function(array, searchItem) {
+ let nextItem = array.length-1; 
+ let tempQueue = []; 
+ while (array[nextItem] != searchItem) {
+	nextItem--;
+ 	let lastItem = array.pop();
+	tempQueue.push(lastItem);
+ }
+
+ if (array.length < 1){
+	 return "ERROR did not find searchItem";
+ }
+ else {
+ 	 array.pop();
+	 while (tempQueue.length > 0){
+		 array.push(tempQueue.shift());
+	 }
+	 return array;
+ }
+ 
 };
 
 // Do not edit below this line

0e80bdff03f2feb4cf4530f201612580b3a61950
Josh Oshiro <joshoshiro99@gmail.com>
2022-09-02 18:33:51 -0400

    Add basic functionality

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit, upperLimit) {
+ let sum = 0;
+ for(let i = lowerLimit; i <= upperLimit; i++){
+	sum += i;
+ }
+ return sum;
 };
 
 // Do not edit below this line

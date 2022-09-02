3c4bb03c2aad40294cd28b8c076b55160582e35f
Josh Oshiro <joshoshiro99@gmail.com>
2022-09-02 18:48:03 -0400

    Add check that limits are in proper order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function(lowerLimit, upperLimit) {
  if (lowerLimit >= 0 && upperLimit >= 0) {
 	 if (typeof lowerLimit == "number" && typeof upperLimit == "number") {
+		 if ( upperLimit < lowerLimit) {
+			 let temp = lowerLimit;
+			 lowerLimit = upperLimit;
+			 upperLimit = temp;
+		 }
 		 let sum = 0;
 		 for(let i = lowerLimit; i <= upperLimit; i++){
 			sum += i;

94762db2c7c0f120c13fe08a191a85b24136e1c1
Josh Oshiro <joshoshiro99@gmail.com>
2022-09-02 20:39:05 -0400

    Add basic functionality to pass first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+ let isLeapYear;	 
+	
+ let isDivisibleBy4 = (year % 4 == 0);
+ console.log(isDivisibleBy4);
+ let isDivisibleBy100 = (year % 100 == 0);
+ console.log(isDivisibleBy100);
+ let isDivisibleBy400 = (year % 400 == 0);
+ console.log(isDivisibleBy400);
+ if (isDivisibleBy4) {
+	 isLeapYear = true;
+	 if (isDivisibleBy100) {
+		 isLeapYear = false;
+		 if (isDivisibleBy400){
+			 isLeapYear = true;
+		 }
+	 }
+	 
+ }
+ else isLeapYear = false;
+ 
+ return isLeapYear;
 };
 
 // Do not edit below this line

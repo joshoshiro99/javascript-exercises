1a28956d6fb67cbb01506307115ccb66861202c3
Josh Oshiro <joshoshiro99@gmail.com>
2022-09-02 20:59:44 -0400

    Add decimal rounding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(tempF) {
  let tempC = (tempF - 32) * (5 / 9);
- return tempC;
+ return Math.round(tempC*10) / 10;
 };
 
 const ctof = function(tempC) {
  let tempF = (tempC * (9 / 5)) + 32;
- return tempF;
+ return Math.round(tempF*10) /10;
 };
 
 // Do not edit below this line
